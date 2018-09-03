415272bc185b01da01afe4be6ac880a26bc88f6f
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-03 20:46:28 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,6 +2,11 @@ class FiguresController < ApplicationController
 
   get '/figures/new' do
     erb :'figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
   end 
+
 
 end
