1669b35593aa0e698641a7777685985455aaf623
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-03 19:18:29 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180903191633) do
+ActiveRecord::Schema.define(version: 20180903191737) do
 
   create_table "figures", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20180903191633) do
     t.string  "name"
     t.integer "figure_id"
     t.integer "year_completed"
+  end
+
+  create_table "titles", force: :cascade do |t|
+    t.string "name"
   end
 
 end
